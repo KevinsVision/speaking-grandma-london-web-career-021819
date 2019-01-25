9b67e46832281a186f9365c7b7412c37a6b13123
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-01-25 02:42:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -20,7 +20,7 @@
       elsif phrase == phrase.downcase
       return "HUH?! SPEAK UP, SONNY!"
       elsif phrase == phrase.upcase
-      return "NO, NOT SINCE 1938"
+      return "NO, NOT SINCE 1938!"
     else
       return"NO, NOT SINCE 1938!"
     end
