c6c2f7e16fa3321c55b8dc5cfb4c989928204c9f
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-01-25 02:35:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,9 +17,9 @@
       return "I LOVE YOU TOO PUMPKIN!"
       elsif phrase == phrase.downcase
       return "HUH?! SPEAK UP, SONNY!"
-      elsif phrase == phrase.upcase
+      elsif phrase == phrase.downcase
       return "HUH?! SPEAK UP, SONNY!"
-      elsif phrase == "WHAT DID YOU EAT TODAY?"
+      elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
       elsif phrase == "WHAT?"
       return "NO, NOT SINCE 1938!"
