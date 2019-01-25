775e88f6d0a123fb960b27123f80e2b54f9b5234
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-01-25 02:28:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,4 +15,9 @@
   def speak_to_grandma
     if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
-      elsif phrase ==
+      elsif phrase == "Hi Nana, how are you?"
+      return "HUH?! SPEAK UP, SONNY!"
+      elsif phrase == "Hi!"
+      return "HUH?! SPEAK UP, SONNY!"
+      elsif phrase == "NO, NOT SINCE 1938!"
+      return ""
